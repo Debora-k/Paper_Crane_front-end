021e0b725a4557148558ed6c89e0ff3301e9cf75
Debora Kwon <95396914+Debora-k@users.noreply.github.com>
2023-02-25 00:34:09 -0700

    feat: added calendar request button and a modal

--- src/pages/employee/emp.cal.page.tsx
+++ src/pages/employee/emp.cal.page.tsx
@@ -1,6 +1,7 @@
 import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from '@fullcalendar/react';
-import React from 'react';
+import { Modal } from 'antd';
+import React, { useState } from 'react';
 
 import './emp.cal.page.css';
 import EmpNavbar from './emp.navbar';
@@ -17,16 +18,43 @@ function renderEventContent(eventInfo) {
 }
 
 const EmpCalendar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleRequest = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div>
       <EmpNavbar />
       <div className='calendar'>
+        {/* This one is a popup after clicking 'Reqeust' button */}
+        <Modal
+          title='Choose day(s) for your time-off request'
+          open={isModalOpen}
+          onOk={handleRequest}
+          onCancel={handleCancel}
+        ></Modal>
         <FullCalendar
           plugins={[dayGridPlugin]}
           initialView='dayGridMonth'
           weekends={true}
           events={events}
           eventContent={renderEventContent}
+          customButtons={{
+            Request: {
+              text: 'Request',
+              click: () => {
+                setIsModalOpen(true);
+              },
+            },
+          }}
+          headerToolbar={{
+            right: 'Request today prev,next',
+          }}
         />
       </div>
     </div>
