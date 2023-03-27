60deb87b21714cf50a50f24d9d05dbf11845c221
Debora-k <95396914+Debora-k@users.noreply.github.com>
2023-03-26 21:28:08 -0600

    feat: added send a proposal and view history of proposals

---
+++ src/dummyData/proposalsHistoryData.ts
@@ -0,0 +1,23 @@
+export const proposalsHistoryData = [
+  {
+    id: 1,
+    subject: 'This is testing subject',
+    sentDate: '2023-04-22',
+    clientEmail: '1234@gmail.com',
+    file: 'http://localhost:3000/testing.doc',
+  },
+  {
+    id: 2,
+    subject: 'This is testing subject',
+    sentDate: '2023-03-30',
+    clientEmail: '12345@gmail.com',
+    link: 'https://www.youtube.com',
+  },
+  {
+    id: 3,
+    subject: 'This is testing subject',
+    sentDate: '2023-02-27',
+    clientEmail: '123456@gmail.com',
+    file: 'http://localhost:3000/testing.doc',
+  },
+];
