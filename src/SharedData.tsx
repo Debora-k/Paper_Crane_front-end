d2857326892bea2305ef9c396a18f7d8cb4bf7f2
Debora Kwon <95396914+Debora-k@users.noreply.github.com>
2023-04-12 16:05:55 -0600

    feat: added Axios for temporary login

--- src/SharedData.tsx
+++ src/SharedData.tsx
@@ -13,6 +13,22 @@ import { TimeOffRequests } from './dummyData/timeoffRequests';
 export const DataContext = createContext<DataContextType | null>(null);
 
 const SharedData: React.FC<any> = ({ children }) => {
+  const [user, setUser] = useState<any>();
+
+  // temporary for testing until frontend has login
+  useEffect(() => {
+    axios
+      .post('http://localhost:8080/authenticate', {
+        email: 'admin1@email.com',
+        password: '123456',
+      })
+      .then((results) => setUser(results.data))
+      .catch((error) => {
+        console.log(error);
+        setUser({ email: 'dummy@gmail.com' });
+      });
+  }, []);
+
   const [admins, setAdmins] = useState<admin | null>(AdminsData);
   const [projects, setProjects] = useState<projectList | null>([]);
   const [timeOffData, setTimeOffData] = useState([]);
@@ -47,6 +63,7 @@ const SharedData: React.FC<any> = ({ children }) => {
 
   // axios for getting employees
   useEffect(() => {
+    if (user === undefined) return;
     axios
       .get('http://localhost:8080/api/v1/employees/')
       .then((results) => setEmployees(results.data))
@@ -55,10 +72,11 @@ const SharedData: React.FC<any> = ({ children }) => {
         // in case back-end isn't connected to front-end, then display dummy data
         setEmployees(EmpData);
       });
-  }, []);
+  }, [user]);
 
   // axios for getting clients
   useEffect(() => {
+    if (user === undefined) return;
     axios
       .get('http://localhost:8080/api/v1/clients/')
       .then((results) => setClients(results.data))
@@ -67,10 +85,11 @@ const SharedData: React.FC<any> = ({ children }) => {
         // in case back-end isn't connected to front-end, then display dummy data
         setClients(AdminClientData);
       });
-  }, []);
+  }, [user]);
 
   // axios for getting time-off requests
   useEffect(() => {
+    if (user === undefined) return;
     axios
       .get('http://localhost:8080/api/v1/time_off_requests/')
       .then((results) => setTimeOffData(results.data))
@@ -79,10 +98,11 @@ const SharedData: React.FC<any> = ({ children }) => {
         // in case back-end isn't connected to front-end, then display dummy data
         setTimeOffData(TimeOffRequests);
       });
-  }, []);
+  }, [user]);
 
   // axios for getting projects list
   useEffect(() => {
+    if (user === undefined) return;
     axios
       .get('http://localhost:8080/api/v1/projects/')
       .then((results) => setProjects(results.data))
@@ -91,7 +111,7 @@ const SharedData: React.FC<any> = ({ children }) => {
         // in case back-end isn't connected to front-end, then display dummy data
         setProjects(projectsData);
       });
-  }, []);
+  }, [user]);
   return (
     <DataContext.Provider
       value={{
